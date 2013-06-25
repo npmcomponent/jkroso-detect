c1c8819195a77829cda5e31be349e29891a2fa19
jkroso <jkroso@gmail.com>
2013-06-25 12:50:13 +1200

    update tests

---
+++ test/chai.js
@@ -0,0 +1,21 @@
+
+var chai = require('chai')
+var Result = require('result')
+
+global.should = chai.should()
+global.expect = chai.expect
+global.assert = chai.assert
+// chai.use(require('chai-spies'))
+
+global.delay = function(value){
+	var result = new Result
+	setTimeout(function(){
+		if (value instanceof Error) result.error(value)
+		else result.write(value)
+	}, Math.random() * 10)
+	return result
+}
+
+chai.Assertion.includeStack = true
+
+module.exports = chai
